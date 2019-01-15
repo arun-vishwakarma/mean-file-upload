557129c8ce4f4488806be6331715da751bb99b0d
Arun Kumar Vishwakarma <mca.arun9@gmail.com>
2019-01-15 18:23:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,14 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended: true}));
  
 app.use(function (req, res, next) {
+  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'POST');
+   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
   //res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
